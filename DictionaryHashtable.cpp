161c92754be7a40cca4e4215e4ce5d1e3d6d8907
Jenny Nguyen <ttn053@ucsd.edu>
2016-02-11 22:38:38 -0800

    1

--- DictionaryHashtable.cpp
+++ DictionaryHashtable.cpp
@@ -1,18 +1,37 @@
 #include "util.hpp"
 #include "DictionaryHashtable.hpp"
+#include <iterator>
 
 /* Create a new Dictionary that uses a Hashset back end */
-DictionaryHashtable::DictionaryHashtable(){}
+DictionaryHashtable::DictionaryHashtable(){
+	this->hashSet = new std::unordered_set<std::string>();
+}
 
 /* Insert a word into the dictionary. */
 bool DictionaryHashtable::insert(std::string word)
 {
+	auto inserted = (this->hashSet)->insert(word);
+
+	if (inserted.second) {
+		return true;
+	}
+
+	else {
+		return false;
+	}
 }
 
 /* Return true if word is in the dictionary, and false otherwise */
 bool DictionaryHashtable::find(std::string word) const
 {
+	auto found = (this->hashSet)->find (word);
+
+	if ( found == )
+
+
 }
 
 /* Destructor */
-DictionaryHashtable::~DictionaryHashtable(){}
+DictionaryHashtable::~DictionaryHashtable(){
+	delete this;
+}
